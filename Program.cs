934f81920a2d30a33df885fd9e5fad1a2a6801bd
akshatanayak007 <56624028+akshatanayak007@users.noreply.github.com>
2022-10-28 12:40:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineBook.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<OnlineBookContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("OnlineBookContext") ?? throw new InvalidOperationException("Connection string 'OnlineBookContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Books}/{action=Index}/{id?}");
+
+app.Run();
